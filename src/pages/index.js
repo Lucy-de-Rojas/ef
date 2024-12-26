c3b21fd83891d6ff1b481b599c1d22bd36cfa11e
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-26 19:38:31 +0000

    added results

--- src/pages/index.js
+++ src/pages/index.js
@@ -155,6 +155,48 @@ program options.</p>
 
 
 
-<div className="w-2/3 bg-blue-300">results</div>
+<div className="w-2/3 px-7">
+<p className="text-2xl font-bold text-center pb-4">Results</p>
+
+<div className="">{data.map((item, index) => { return <div className="wrapperForResults" key={index}>
+
+<div className="itemWrapper bg-purple-200 my-6 py-7 rounded-lg px-5 shadow-md">
+
+
+<div className="titleSection flex flex-row justify-between">
+
+ <span className="font-bold"> {item.title}</span>  {item.bestseller && <span> 🔥 Bestseller </span>}
+</div>
+
+<div className="spacer"> <hr  className="h-3/4 bg-black rounded-full mb-4 mt-1"/></div>
+
+<div className="bottomSection flex flex-row justify-between">
+  <div className="topicWrapper">
+    {item.topic.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
+  </div>
+
+<div className="learningFormatsWrapper flex flex-row ">
+
+
+
+{item.learningFormats.map((format, index) => (
+  <span key={index} className="">
+    {format.charAt(0).toUpperCase() + format.slice(1)}
+    {index < item.learningFormats.length - 1 && <span> • </span>}
+  </span>
+))}
+
+
+
+</div>
+
+
+</div>
+</div>
+
+  </div>})}</div>
+
+
+</div>
 
   </div></div>);}
