6189a2dc9594aa0d9534e68e6f621a4b9c195dda
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-30 19:35:59 +0000

    sorting az

--- src/components/azswitch.js
+++ src/components/azswitch.js
@@ -17,8 +17,8 @@ export default function AZSwitch ({setAZSorting}) {
 
 return (<div className="w-3/4 flex justify-between ">
 
-<input type="button" value="A-Z ⬇️" className="bg-gray-500 w-[45%]  text-white mx-1 rounded-full" onClick={handleAZ} id="az" />
-<input type="button" value="Z-A ⬆️"  className="bg-gray-500 w-[45%] text-white mx-1 rounded-full" onClick={handleZA} id="za" />
+<input type="button" value="A-Z ⬇️" className="bg-gray-500 w-[45%] lg:py-5  text-white mx-1 rounded-full" onClick={handleAZ} id="az" />
+<input type="button" value="Z-A ⬆️"  className="bg-gray-500 w-[45%] lg:py-5  text-white mx-1 rounded-full" onClick={handleZA} id="za" />
 
 
 </div>)};
