eef16bb3df106ce97c10e841b6e04d4c43a8799c
Lucy de Rojas <lucyderojas@gmail.com>
2025-01-07 17:32:15 +0000

    fixed mobile screen fitting of az switch

--- src/components/azswitch.js
+++ src/components/azswitch.js
@@ -15,10 +15,10 @@ export default function AZSwitch ({setAZSorting}) {
 
 
 
-return (<div className="w-3/4 flex justify-between ">
+return (<div className="w-3/4 flex flex-col lg:flex-row lg:justify-center  items-center">
 
-<input type="button" value="A-Z ⬇️" className="bg-gray-500 w-[45%] lg:py-5  text-white mx-1 rounded-full" onClick={handleAZ} id="az" />
-<input type="button" value="Z-A ⬆️"  className="bg-gray-500 w-[45%] lg:py-5  text-white mx-1 rounded-full" onClick={handleZA} id="za" />
+<input type="button" value="A-Z ⬇️" className="bg-gray-500 w-[70%] lg:w-[45%] py-4 mb-2 lg:mb-0  text-white mx-1 rounded-full" onClick={handleAZ} id="az" />
+<input type="button" value="Z-A ⬆️"  className="bg-gray-500 w-[70%] lg:w-[45%] py-4  text-white mx-1 rounded-full" onClick={handleZA} id="za" />
 
 
 </div>)};
